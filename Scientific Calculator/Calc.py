226753380ec4666e58f9cefe77fc4c47d3af082a
Orlando Rolim <rolimorlando@gmail.com>
2022-01-04 10:41:24 -0300

    Update Calculator GUI Project

--- Scientific Calculator/Calc.py
+++ Scientific Calculator/Calc.py
@@ -21,7 +21,7 @@ def equalpress():
         expression = ""
 
     except:
-        equation.set(" error ")
+        equation.set("error")
         expression = ""
 
 
@@ -34,82 +34,76 @@ def clear():
 # Driver code
 if __name__ == "__main__":
     gui = Tk()
-    gui.configure(background="light green")
+    gui.configure(background="gray")
     gui.title("Simple Calculator")
-    gui.geometry("320x250")
+    gui.geometry("260x205")
 
     equation = StringVar()
 
-    expression_field = Entry(gui, textvariable=equation)
-    expression_field.grid(columnspan=40, ipadx=88, ipady=10)
+    expression_field = Entry(gui, width=40, state='readonly', background='red', textvariable=equation)
+    expression_field.grid(row=0, columnspan=10, ipadx=6, ipady=8)
+    expression_field.focus()
 
-    button1 = Button(gui, text=' 1 ', fg='black', bg='red', command=lambda: press(1),
-                     height=2, width=10)
-    button1.grid(row=2, column=0)
 
-    button2 = Button(gui, text=' 2 ', fg='black', bg='red', command=lambda: press(2),
-                     height=2, width=10)
-    button2.grid(row=2, column=1)
+    # COLUMN 1 - 7 8 9 #
+    button7 = Button(gui, text=' 7 ', fg='white', bg='black', command=lambda: press(7), width=8)
+    button7.grid(row=1, column=0, ipady=4, ipadx=2)
 
-    button3 = Button(gui, text=' 3 ', fg='black', bg='red', command=lambda: press(3),
-                     height=2, width=10)
-    button3.grid(row=2, column=2)
+    button8 = Button(gui, text=' 8 ', fg='white', bg='black', command=lambda: press(8), width=8)
+    button8.grid(row=1, column=1, ipady=4, ipadx=2)
 
-    button4 = Button(gui, text=' 4 ', fg='black', bg='red', command=lambda: press(4),
-                     height=2, width=10)
-    button4.grid(row=3, column=0)
+    button9 = Button(gui, text=' 9 ', fg='white', bg='black', command=lambda: press(9), width=8)
+    button9.grid(row=1, column=2, ipady=4, ipadx=2)
 
-    button5 = Button(gui, text=' 5 ', fg='black', bg='red', command=lambda: press(5),
-                     height=2, width=10)
-    button5.grid(row=3, column=1)
 
-    button6 = Button(gui, text=' 6 ', fg='black', bg='red', command=lambda: press(6),
-                     height=2, width=10)
-    button6.grid(row=3, column=2)
+    # COLUMN 2 - 4 5 6 #
+    button4 = Button(gui, text=' 4 ', fg='white', bg='black', command=lambda: press(4), width=8)
+    button4.grid(row=2, column=0, ipady=4, ipadx=2)
 
-    button7 = Button(gui, text=' 7 ', fg='black', bg='red', command=lambda: press(7),
-                     height=2, width=10)
-    button7.grid(row=4, column=0)
+    button5 = Button(gui, text=' 5 ', fg='white', bg='black', command=lambda: press(5), width=8)
+    button5.grid(row=2, column=1, ipady=4, ipadx=2)
 
-    button8 = Button(gui, text=' 8 ', fg='black', bg='red', command=lambda: press(8),
-                     height=2, width=10)
-    button8.grid(row=4, column=1)
+    button6 = Button(gui, text=' 6 ', fg='white', bg='black', command=lambda: press(6), width=8)
+    button6.grid(row=2, column=2, ipady=4, ipadx=2)
 
-    button9 = Button(gui, text=' 9 ', fg='black', bg='red', command=lambda: press(9),
-                     height=2, width=10)
-    button9.grid(row=4, column=2)
 
-    button0 = Button(gui, text=' 0 ', fg='black', bg='red', command=lambda: press(0),
-                     height=2, width=10)
-    button0.grid(row=5, column=1)
+    # COLUMN 3 - 1 2 3 #
+    button1 = Button(gui, text=' 1 ', fg='white', bg='black', command=lambda: press(1), width=8)
+    button1.grid(row=3, column=0, ipady=4, ipadx=2)
 
-    plus = Button(gui, text=' + ', fg='black', bg='red', command=lambda: press("+"),
-                  height=2, width=10)
-    plus.grid(row=2, column=3)
+    button2 = Button(gui, text=' 2 ', fg='white', bg='black', command=lambda: press(2), width=8)
+    button2.grid(row=3, column=1, ipady=4, ipadx=2)
 
-    minus = Button(gui, text=' - ', fg='black', bg='red', command=lambda: press("-"),
-                   height=2, width=10)
-    minus.grid(row=3, column=3)
+    button3 = Button(gui, text=' 3 ', fg='white', bg='black', command=lambda: press(3), width=8)
+    button3.grid(row=3, column=2, ipady=4, ipadx=2)
 
-    multiply = Button(gui, text=' * ', fg='black', bg='red', command=lambda: press("*"),
-                      height=2, width=10)
-    multiply.grid(row=4, column=3)
 
-    divide = Button(gui, text=' / ', fg='black', bg='red', command=lambda: press("/"),
-                    height=2, width=10)
-    divide.grid(row=5, column=3)
+    # COLUMN 4 - Clean 0 . #
+    button_Clear = Button(gui, text=' C ', fg='white', bg='red', command=clear, width=8)
+    button_Clear.grid(row=4, column=0, ipady=4, ipadx=2)
 
-    equal = Button(gui, text=' = ', fg='black', bg='red', command=equalpress,
-                   height=2, width=10)
-    equal.grid(row=5, column=2)
+    button0 = Button(gui, text=' 0 ', fg='white', bg='black', command=lambda: press(0), width=8)
+    button0.grid(row=4, column=1, ipady=4, ipadx=2)
 
-    clear = Button(gui, text='Clear', fg='black', bg='red', command=clear,
-                   height=2, width=10)
-    clear.grid(row=5, column='0')
+    button_dec = Button(gui, text=' . ', fg='white', bg='black', command=lambda: press('.'), width=8)
+    button_dec.grid(row=4, column=2, ipady=4, ipadx=2)
 
-    Decimal = Button(gui, text='.', fg='black', bg='red', command=lambda: press('.'),
-                     height=2, width=10)
-    Decimal.grid(row=6, column=1)
+    # COLUMN 5 - = #
+    button_equal = Button(gui, text=' = ', fg='white', bg='green', command=equalpress, width=8)
+    button_equal.grid(row=5, column=1, ipady=4, ipadx=2)
 
+
+    # GRID 4 #
+    button_plus = Button(gui, text=' + ', fg='white', bg='black', command=lambda: press("+"), width=5)
+    button_plus.grid(row=1, column=3, ipady=4, ipadx=2)
+
+    button_minus = Button(gui, text=' - ', fg='white', bg='black', command=lambda: press("-"), width=5)
+    button_minus.grid(row=2, column=3, ipady=4, ipadx=2)
+
+    button_mult = Button(gui, text=' * ', fg='white', bg='black', command=lambda: press("*"), width=5)
+    button_mult.grid(row=3, column=3, ipady=4, ipadx=2)
+
+    button_div = Button(gui, text=' / ', fg='white', bg='black', command=lambda: press("/"), width=5)
+    button_div.grid(row=4, column=3, ipady=4, ipadx=2)
 
     gui.mainloop()
